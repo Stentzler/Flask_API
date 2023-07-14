affa559aa4a6bce6f43171a0a5602bdc7ed33f96
Vinicius Stentzler <vin.stentzler@gmail.com>
2023-07-14 19:48:16 -0300

    feat: UUID

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
-
-stores = [{"name": "MyStore", "items": [{"name": "Chair", "price": 19.99}]}]
 
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
     return new_store, 201
+
+
+# Params
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+
+    if item_data["store_id"] in stores:
+        item_id = uuid.uuid4().hex
+        items[item_id] = {**item_data, "id": item_id}
+        return items[item_id], 201
+
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/item")
+def get_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_store_by_id(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
+
+
+@app.get("/item/<string:item_id>")
+def get_store_items(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
